e1d1a38ebfe7a9c72715fe8dec7259ac83fcbce7
Ivan Szebenszki <szib77@gmail.com>
2019-03-04 17:45:00 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,3 +16,12 @@ def move(board, idx, player = "X")
   board[idx] = player
   return board
 end 
+
+def display_board(board)
+  separator = "-" * 11
+  printed_board = board.map { |tile| " #{tile} " }
+    .map.with_index { |tile, idx| idx % 3 === 1 ? "|#{tile}|" : tile }
+    .map!.with_index { |tile, idx| [2,5].include?(idx) ? "#{tile}\n#{separator}\n" : tile }
+    .join
+  puts printed_board
+end
