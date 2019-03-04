688303ab5330fe236d4db6aa98c28d40c8cceabd
Ivan Szebenszki <szib77@gmail.com>
2019-03-04 17:41:50 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,18 @@
 def valid_move?(board, idx)
-  return true if (0..8).include?(idx) && !position_taken?(board, idx)	  return true if (0..8).include?(idx) && !position_taken?(board, idx)
+  return true if (0..8).include?(idx) && !position_taken?(board, idx)
   false
-end	end
+end
 
 def position_taken?(board, idx)	def position_taken?(board, idx)
-  return true unless (board[idx] == " " || board[idx] == "" || board[idx] == nil)	  return true unless (board[idx] == " " || board[idx] == "" || board[idx] == nil)
+  return true unless (board[idx] == " " || board[idx] == "" || board[idx] == nil)
   false
-end 	end 
+end
+
+def input_to_index(number)
+   number.to_i - 1
+end
+
+ def move(board, idx, player = "X")
+  board[idx] = player
+  return board
+end 
